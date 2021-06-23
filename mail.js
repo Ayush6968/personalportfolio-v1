99db3efdd705f0dd2bbe39ebd4d241d7da55ec3a
Ayush <ayush.gupta.jk@gmail.com>
2021-06-23 17:23:14 +0530

    Added enviornment variable for api key

--- mail.js
+++ mail.js
@@ -1,34 +1,33 @@
 const nodemailer = require('nodemailer');
 const mailgun = require('nodemailer-mailgun-transport');
 
-const auth={
-auth: {
-  api_key:"key-e3fb6cc055f75a4ff2ca3798babc35b3",
-  domain:"sandboxaa7b20b1cdfb4301a28ff52f5480a372.mailgun.org",
-}
+require('dotenv').config();
+
+console.log(process.env);
+
+const auth = {
+	auth: {
+		api_key: process.env.API_KEY,
+		domain: 'sandboxaa7b20b1cdfb4301a28ff52f5480a372.mailgun.org',
+	},
 };
 const transporter = nodemailer.createTransport(mailgun(auth));
 
+const sendMail = (name, subject, email, message, callback) => {
+	let mailOptions = {
+		to: 'agupta17_be18@thapar.edu',
+		name,
+		subject: subject,
+		from: email,
+		text: message,
+	};
 
-const sendMail=(name,subject,email,message,callback)=>{
-let mailOptions = {
-    to: 'tommysingh460@gmail.com',
-    name,
-    subject:subject, 
-    from:email,
-    text:message,
-    
-  
+	transporter.sendMail(mailOptions, (error, data) => {
+		if (error) {
+			return callback(error, null);
+		}
+		callback(null, data);
+	});
 };
 
-
-transporter.sendMail(mailOptions, (error, data) => {
-    if (error) {
-     return callback(error,null)
-    }
-    callback(null,data)
-});
-
-}
-
-module.exports= sendMail;
+module.exports = sendMail;
