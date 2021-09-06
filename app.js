e78c0a22844658885e5de49c2e990d106b9cde95
Ayush <ayush.gupta.jk@gmail.com>
2021-09-06 18:26:46 +0530

    fixed some errors

--- app.js
+++ app.js
@@ -1,56 +1,59 @@
-const express = require("express");
-const hbs = require("hbs");
-const path = require("path");
-var bodyParser = require("body-parser");
-const sendMail = require("./mail");
-const { send } = require("process");
-const { Console } = require("console");
+/** @format */
+
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+var bodyParser = require('body-parser');
+const sendMail = require('./mail');
+const { send } = require('process');
+const { Console } = require('console');
 const port = process.env.PORT || 3000;
 
 const app = express();
 
-const assets = path.join(__dirname, "/public");
+const assets = path.join(__dirname, '/public');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(assets));
 // TO SET TEMPLATE ENGINE
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "./templates/views"));
-const partialspath = path.join(__dirname, "./templates/partials");
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, './templates/views'));
+const partialspath = path.join(__dirname, './templates/partials');
 hbs.registerPartials(partialspath);
 
-app.get("/", (req, res) => {
-  res.render("home");
+app.get('/', (req, res) => {
+	res.render('home');
 });
 
-app.get("/about", (req, res) => {
-  res.render("about");
+app.get('/about', (req, res) => {
+	res.render('about');
 });
 
-app.get("/contact", (req, res) => {
-  res.render("contact");
+app.get('/contact', (req, res) => {
+	res.render('contact');
 });
-app.get("/work", (req, res) => {
-  res.render("work");
+app.get('/work', (req, res) => {
+	res.render('work');
 });
-app.get("/sucess", (req, res) => {
-  res.render("sucess");
+app.get('/sucess', (req, res) => {
+	res.render('sucess');
 });
-app.post("/contact", (req, res) => {
-  const { Name, Subject, Email, Message } = req.body;
-  sendMail(Name, Subject, Email, Message, (error, data) => {
-    if (error) {
-      return res.redirect("/404");
-    }
-    res.redirect("/sucess");
-  });
+app.post('/contact', (req, res) => {
+	const { Name, Subject, Email, Message } = req.body;
+	sendMail(Name, Subject, Email, Message, (error, data) => {
+		if (error) {
+			console.log(error);
+			return res.redirect('/404');
+		}
+		res.redirect('/sucess');
+	});
 });
 
-app.get("/*", (req, res) => {
-  res.render("404");
+app.get('/*', (req, res) => {
+	res.render('404');
 });
 
 app.listen(port, () => {
-  console.log("server is running on" + port);
+	console.log('server is running on' + port);
 });
--- mail.js
+++ mail.js
@@ -1,34 +1,32 @@
+/** @format */
+
 const nodemailer = require('nodemailer');
 const mailgun = require('nodemailer-mailgun-transport');
+require('dotenv').config();
 
-const auth={
-auth: {
-  api_key:"key-e3fb6cc055f75a4ff2ca3798babc35b3",
-  domain:"sandboxaa7b20b1cdfb4301a28ff52f5480a372.mailgun.org",
-}
+const auth = {
+	auth: {
+		api_key: `${process.env.API_KEY}`,
+		domain: `${process.env.DOMAIN_NAME}`,
+	},
 };
 const transporter = nodemailer.createTransport(mailgun(auth));
 
+const sendMail = (name, subject, email, message, callback) => {
+	let mailOptions = {
+		to: 'tommysingh460@gmail.com',
+		name,
+		subject: subject,
+		from: email,
+		text: message,
+	};
 
-const sendMail=(name,subject,email,message,callback)=>{
-let mailOptions = {
-    to: 'tommysingh460@gmail.com',
-    name,
-    subject:subject, 
-    from:email,
-    text:message,
-    
-  
+	transporter.sendMail(mailOptions, (error, data) => {
+		if (error) {
+			return callback(error, null);
+		}
+		callback(null, data);
+	});
 };
 
-
-transporter.sendMail(mailOptions, (error, data) => {
-    if (error) {
-     return callback(error,null)
-    }
-    callback(null,data)
-});
-
-}
-
-module.exports= sendMail;
+module.exports = sendMail;
